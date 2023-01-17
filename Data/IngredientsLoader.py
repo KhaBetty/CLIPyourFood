3afbc3e1f8921369eed24b03a9aba657266337e5
KhaBetty <KhaBetty@users.noreply.github.com>
2023-01-17 23:01:44 +0200

    added the correct way to load the data

---
+++ Data/IngredientsLoader.py
@@ -0,0 +1,52 @@
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+import json
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+plt.ion()
+
+
+class IngredientsDataset(Dataset):
+	"""Ingredients dataset."""
+
+	def __init__(self, json_file, root_dir, transform=None, img_ext='.jpg'):
+		"""
+		Args:
+			json_file (string): Path to the json file with annotations.
+			root_dir (string): Directory with all the images.
+			transform (callable, optional): Optional transform to be applied
+				on a sample.
+		"""
+		self.ingredients_frame = pd.read_json(json_file)
+		self.root_dir = root_dir
+		self.transform = transform
+		self.images = list(self.ingredients_frame.columns)
+		self.img_ext = img_ext
+
+	def __len__(self):
+		return len(self.images)
+
+	def __getitem__(self, idx):
+		if torch.is_tensor(idx):
+			idx = idx.tolist()
+		img_name = self.images[idx]
+		img_path = os.path.join(self.root_dir, img_name + self.img_ext)
+		image = io.imread(img_path)
+		ingredients = list(self.ingredients_frame[img_name])
+		sample = {'image': image, 'ingredients': ingredients}
+
+		if self.transform:
+			sample['image'] = self.transform(sample['image'])
+
+		return sample
+
---
+++ Data/main.py
@@ -0,0 +1,65 @@
+# This is a sample Python script.
+import numpy as np
+import json5 as json
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    #parameters:
+    ingredients_file = "C:\DL_local\Annotations\ingredients_simplified.txt"
+    food_img_file = "C:\DL_local\Annotations\img_train.txt"
+    slash = "\\"
+
+
+    # open the text file of the ingredients
+    with open(ingredients_file, 'r') as f_ing:
+        # read the ingredients text file into a list of lines
+        ing_lines = f_ing.readlines()
+
+    # open the text file of the food
+    with open(food_img_file, 'r') as f_food:
+        # read the ingredients text file into a list of lines
+        food_lines = f_food.readlines()
+
+    # create an food dictionary
+    food_dict = {}
+
+    for img in food_lines:
+        # remove the \n from the end of the line:
+        img = img[:-1]
+        img_file_name = img.split("/")
+        #split into food class and id:
+        food_class, id = img_file_name
+        food_dict.setdefault(food_class, [])
+        food_dict[food_class].append(id)
+
+    # create an empty dictionary for the ingredients
+    ing_dict = {}
+
+
+    # loop through the lines in the text file
+    for line in ing_lines:
+        #remove the \n from the end of the line:
+        line = line[:-1]
+        # split the line on ':'
+        value = line.split(',')
+        for food_class in food_dict:
+            for id in food_dict[food_class]:
+                key = str(food_class) + slash + str(id)
+                #print(key) # for debug
+                ing_dict[key] = value
+
+
+    with open("../food101/train/food-101/images/ing_jsn.json", "w") as outfile:
+        json.dump(ing_dict, outfile)
